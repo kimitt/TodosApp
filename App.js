22954c29965e79061da79814ffd50c24b163dc99
misung Kim <mizzkkim7@gmail.com>
2022-05-23 16:16:00 +0900

    FEAT: AsyncStorage 저장기능 구현 :앱 종료수 다시 접속해도 입력한 텍스트 스토리지에 저장됨

--- App.js
+++ App.js
@@ -1,25 +1,39 @@
-import React,  { useState } from 'react';
+import React,  { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TouchableOpacity, TextInput, ScrollView, } from 'react-native';
 import { theme } from './Colors';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
+const STORAGE_KEY = "@toDos";
 
 export default function App() {
   const [working, setWorking] = useState(true);
   const [text, setText] = useState("");
   const [toDos, setToDos] = useState({});
+  useEffect(() => {
+    loadToDos();
+  }, []);
   const play = () => setWorking(false);
   const work = () => setWorking(true);
   const onChangeText = (payload) => setText(payload);
-  const addTodo = () => {
+  const saveToDos = async (toSave) => {
+    await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(toSave));
+  };
+  const loadToDos = async () => {
+    const s = await AsyncStorage.getItem(STORAGE_KEY);
+    console.log(s);
+    s !== null ? setToDos(JSON.parse(s)) : null;
+  };
+  const addToDo = async () => {
     if (text === "") {
       return;
     }
     const newToDos = {
       ...toDos,
-      [Date.now()]: { text, work: working },
+      [Date.now()]: { text, working },
     };
     setToDos(newToDos);
+    await saveToDos(newToDos);
     setText("");
   }
   return (
@@ -42,7 +56,7 @@ export default function App() {
         </TouchableOpacity>
       </View>
       <TextInput
-        onSubmitEditing={addTodo}
+        onSubmitEditing={addToDo}
         onChangeText={onChangeText}
         returnKeyType="done"
         value={text}
@@ -50,11 +64,13 @@ export default function App() {
         style={styles.input}
       />
       <ScrollView>
-        {Object.keys(toDos).map((key) => (
+        {Object.keys(toDos).map((key) => 
+          toDos[key].working === working ? (
           <View style={styles.toDo} key={key}>
             <Text style={styles.toDoText}>{toDos[key].text}</Text>
           </View>
-        ))}
+          ) : null
+        )}
       </ScrollView>
     </View>
   );
